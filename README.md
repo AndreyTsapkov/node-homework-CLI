71ec78f021e88571c755b5861525d27e1fe6e096
Andrey Tsapkov <tsapkov1994@gmail.com>
2022-11-05 16:48:22 +0200

    hw

---
+++ README.md
@@ -0,0 +1,29 @@
+# Получаем и выводим весь список контактов в виде таблицы
+
+node index.js --action list
+
+https://monosnap.com/file/ndzLAZtc5BQnDHw8yD3k9EwpiSxeEh
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+
+https://monosnap.com/file/kMEf37U8pyxFdIsLpXKnGV52NQoOkJ
+
+# Добавляем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+https://monosnap.com/file/V2IQClI9byXYTNgeBHtcmZQHe8acBG
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+
+https://monosnap.com/file/7qgVICB6rjs2e7XPNXmmvW5nl4szco
+
+# Повторно получаем и выводим весь список контактов в виде таблицы, комманды сработали корректно
+
+node index.js --action list
+
+https://monosnap.com/file/QcB9WXrrFCGtZKhGPZz1VZK9LyvFcG
--- contacts.js
+++ contacts.js
@@ -1,22 +1,59 @@
 const fs = require("fs").promises;
 const path = require("path");
 const { nanoid } = require("nanoid");
+const { writeFile } = require("fs");
 const contactsPath = path.resolve("./db/contacts.json");
 
-function listContacts() {
-  // ...твой код
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    console.table(JSON.parse(data));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contact = JSON.parse(data).find(({ id }) => id === contactId);
+    console.table(contact);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const newContactsList = JSON.parse(data).filter(
+      ({ id }) => id !== contactId
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(newContactsList), "utf-8");
+    console.log("Contact deleted");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contactsList = JSON.parse(data);
+    const newContactsList = [...contactsList, newContact];
+
+    await fs.writeFile(contactsPath, JSON.stringify(newContactsList), "utf-8");
+    console.log(`${name} added to contacts`);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
